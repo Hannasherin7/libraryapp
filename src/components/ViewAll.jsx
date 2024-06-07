f3797a486087703dca9ddcf740976a30a93a068c
hannasherin <hannasherin7110@gmail.com>
2024-06-07 14:21:48 +0530

    message

--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import { NavBar } from './NavBar'
 
 export const ViewAll = () => {
   return (
     <div>
+        <NavBar></NavBar>
         <div className="container">
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     
-                <table class="table">
+                <table class="table" border="2">
   <thead>
     <tr>
       <th scope="col">TITLE</th>
